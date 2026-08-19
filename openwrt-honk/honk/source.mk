433756556f76817894b46d3d0583c411d693ab4b
Hdragon112 <noodles@163.com>
2026-08-19 16:38:45 +0000

    Update 2026/08/19 16:38:45 UTC

--- openwrt-honk/honk/source.mk
+++ openwrt-honk/honk/source.mk
@@ -1,5 +1,5 @@
 # Generated from locks/source.lock.json. Do not edit.
-PKG_SOURCE_VERSION:=5f4cc1a1411387742bc1fce63962a2a86cb9e00a
-PKG_SOURCE:=5f4cc1a1411387742bc1fce63962a2a86cb9e00a.tar.gz
+PKG_SOURCE_VERSION:=41ec6018552421e11d9783163709034a1d51525e
+PKG_SOURCE:=41ec6018552421e11d9783163709034a1d51525e.tar.gz
 PKG_SOURCE_URL:=https://github.com/Glassyiris/honk/archive
 PKG_MIRROR_HASH:=skip
